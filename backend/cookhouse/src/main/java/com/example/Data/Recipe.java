3c09eb978ed9841d22ff3647a01068eb94cd2d0f
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-05-01 23:57:14 +0800

    deleted instruction model and updated it as an array of String in Recipe.java

--- backend/cookhouse/src/main/java/com/example/Data/Recipe.java
+++ backend/cookhouse/src/main/java/com/example/Data/Recipe.java
@@ -3,8 +3,11 @@ package com.example.Data;
 import java.util.ArrayList;
 
 public class Recipe {
+    private String id;
+    private String name;
+    private User author;
     private ArrayList<Ingredient> ingredients;
     private ArrayList<Tool> tools;
-    private ArrayList<Instruction> instructions;
+    private ArrayList<String> instructions;
     private Nutrition nutrition;
 }

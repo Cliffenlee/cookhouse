7c6f04a5aeebde978b2dc21032f54f87cffdef2d
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2021-05-01 15:14:50 +0800

    initialized backend springboot application

---
+++ backend/cookhouse/src/main/java/com/example/cookhouse/CookhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookhouseApplication.class, args);
+	}
+
+}
---
+++ backend/cookhouse/src/test/java/com/example/cookhouse/CookhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d81d09ba51e41d786c81ea4b9ef53d326d4eb1bf
CliffenLee <cliffen.lee.2018@sis.smu.edu.sg>
2021-05-04 12:00:27 +0800

    added error message object for users not found

--- backend/cookhouse/src/main/java/com/example/controller/UserController.java
+++ backend/cookhouse/src/main/java/com/example/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.example.controller;
 
+import com.example.data.models.Error;
 import com.example.data.models.User;
 import com.example.data.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -19,7 +19,7 @@ public class UserController {
 
     // get all users
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity getAllUsers() {
         try {
             List<User> userList = (List<User>)userRepository.findAll();
             if (userList.isEmpty() || userList.size() == 0) {
@@ -28,38 +28,38 @@ public class UserController {
 
             return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Error>(new Error(500, e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     // get single user
     @GetMapping("/user/{id}")
-    public ResponseEntity<User> getUser(@PathVariable Integer id) {
+    public ResponseEntity getUser(@PathVariable Integer id) {
         try {
             Optional<User> user = userRepository.findById(id);
             if (user.isPresent()) {
                 return new ResponseEntity<User>(user.get(), HttpStatus.OK);
             }
 
-            return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<Error>(new Error(404, "user id not found: " + id),HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Error>(new Error(500, e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     // create user
     @PostMapping("/user")
-    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+    public ResponseEntity createUser(@Valid @RequestBody User user) {
         try {
             return new ResponseEntity<User>(userRepository.save(user), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Error>(new Error(500, e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     // delete user
     @DeleteMapping("/user/{id}")
-    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Integer id) {
+    public ResponseEntity deleteUser(@PathVariable Integer id) {
         try {
             Optional<User> user = userRepository.findById(id);
             if (user.isPresent()) {
@@ -67,18 +67,25 @@ public class UserController {
                 return new ResponseEntity<HttpStatus>(HttpStatus.OK);
             }
 
-            return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<Error>(new Error(404, "user id not found: " + id), HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<HttpStatus>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Error>(new Error(500, e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    // update user
     @PutMapping("/user/{id}")
-    public ResponseEntity<User> updateUser (@PathVariable Integer id, @Valid @RequestBody User user) {
+    public ResponseEntity updateUser (@PathVariable Integer id, @Valid @RequestBody User newUser) {
         try {
-            return new ResponseEntity<User>(userRepository.save(user), HttpStatus.OK);
+            Optional<User> user = userRepository.findById(id);
+            if (user.isPresent()) {
+                // update user
+                return new ResponseEntity<User>(user.get(), HttpStatus.OK);
+            }
+
+            return new ResponseEntity<Error>(new Error(404, "user id not found: " + id), HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Error>(new Error(500, e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
---
+++ backend/cookhouse/src/main/java/com/example/data/models/Error.java
@@ -0,0 +1,13 @@
+package com.example.data.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Error {
+    private int status;
+    private String error;
+}
